9eb8abf4baefb0b43dc64cf227be68d11c531851
COSTAFA <frederico.costa@eversource.com>
2024-11-07 17:50:29 +0000

    use new library WebDriveWait and code format

--- src/app.py
+++ src/app.py
@@ -1,34 +1,32 @@
-import openpyxl.workbook
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import openpyxl
 
 driver = webdriver.Chrome()
 driver.get('https://www.idealista.pt/comprar-casas/lisboa/')
 
 titles = driver.find_elements(By.XPATH, "//a[@class='item-link ']")
-
-# for test purpose 
-""" for title in titles: 
-    print(title.text) """
-
 prices = driver.find_elements(By.XPATH, "//span[@class='item-price h2-simulated']")
 
-# for test purpose
-""" for price in prices:
-    print(price.text) """
+# for test purpose 
+for title in titles: 
+    print(title.text)
+
+for price in prices:
+    print(price.text)
 
 try:
     buttons = driver.find_elements(By.XPATH, "//button[@class='icon-phone hidden-contact-phones_link see-phones-btn fake-anchor']")
 
     for button in buttons:
-        ActionChains(driver).move_to_element(button).click(button).perform
-        time.sleep(10)
-        print(button)
-        print("_________")
-        print(button.text)
+        ActionChains(driver).move_to_element(button).click(button).perform()
+
+        WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, "//span[@class='hidden-contact-phones_text']"))
+        )
 
     numbers = driver.find_elements(By.XPATH, "//span[@class='hidden-contact-phones_text']")
 
@@ -36,16 +34,15 @@ try:
         print(number.text)
 
 finally:
-
     driver.quit()
 
-""" workbook = openpyxl.workbook()
+workbook = openpyxl.Workbook()
 workbook.create_sheet('housesIdealista')
 sheet_houses = workbook['housesIdealista']
 
 sheet_houses['A1'].value = 'Descrição'
 sheet_houses['B1'].value = 'Preço'
-sheet_houses['C1'].value = 'Telefone' """
+sheet_houses['C1'].value = 'Telefone'
 
 
 
