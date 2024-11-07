8fe6a967fd47b1759781246c35f50c89d974954f
COSTAFA <frederico.costa@eversource.com>
2024-11-07 14:25:54 +0000

    new try catch for click in the button automatic

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 import openpyxl.workbook
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+import time
 import openpyxl
 
 driver = webdriver.Chrome()
@@ -18,20 +20,32 @@ prices = driver.find_elements(By.XPATH, "//span[@class='item-price h2-simulated'
 """ for price in prices:
     print(price.text) """
 
-phoneNumber = driver.find_elements(By.XPATH, "//span[@class='item-price h2-simulated']")
+try:
+    buttons = driver.find_elements(By.XPATH, "//button[@class='icon-phone hidden-contact-phones_link see-phones-btn fake-anchor']")
 
-# for test purpose
-""" for price in prices:
-    print(price.text) """
+    for button in buttons:
+        ActionChains(driver).move_to_element(button).click(button).perform
+        time.sleep(10)
+        print(button)
+        print("_________")
+        print(button.text)
 
+    numbers = driver.find_elements(By.XPATH, "//span[@class='hidden-contact-phones_text']")
 
-workbook = openpyxl.workbook()
+    for number in numbers:
+        print(number.text)
+
+finally:
+
+    driver.quit()
+
+""" workbook = openpyxl.workbook()
 workbook.create_sheet('housesIdealista')
 sheet_houses = workbook['housesIdealista']
 
 sheet_houses['A1'].value = 'Descrição'
 sheet_houses['B1'].value = 'Preço'
-sheet_houses['C1'].value = 'Telefone'
+sheet_houses['C1'].value = 'Telefone' """
 
 
 
